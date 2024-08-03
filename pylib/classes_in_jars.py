320bb35a8744008674ab59e73e796ce6877f6ab1
KimuraShinichi <kimura.shinichi@ieee.org>
2024-08-03 17:31:31 +0900

    lib-extracted とするべき箇所が lib-extract だったので訂正しました。

--- pylib/classes_in_jars.py
+++ pylib/classes_in_jars.py
@@ -2,21 +2,21 @@
 
 import os
 
-def find_and_transform(lib_extract):
+def find_and_transform(lib_extracted):
     result = []
-    for root, dirs, files in os.walk(lib_extract):
+    for root, dirs, files in os.walk(lib_extracted):
         for file in files:
             file_path = os.path.join(root, file)
-            transformed_path = file_path.replace(lib_extract + '/', '', 1).replace('.extracted/', '\t')
+            transformed_path = file_path.replace(lib_extracted + '/', '', 1).replace('.extracted/', '\t')
             result.append(transformed_path)
     return result
 
-def main(lib_extract):
-    transformed_paths = find_and_transform(lib_extract)
+def main(lib_extracted):
+    transformed_paths = find_and_transform(lib_extracted)
     for path in transformed_paths:
         print(path)
 
 if __name__ == "__main__":
-    lib_extract = '.lib-extract'  # Replace with your directory if needed
+    lib_extract = '.lib-extracted'  # Replace with your directory if needed
     main(lib_extract)
 
